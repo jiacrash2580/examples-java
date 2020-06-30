b1d2b2d562bfba95df0477808df970ac7ffe14aa
jiaxiaochen <jiaxc@beic.gov.cn>
2020-06-30 15:25:41 +0800

    tag

--- src/main/java/io/github/streamingwithflink/test/tablesql/TableSqlTest.java
+++ src/main/java/io/github/streamingwithflink/test/tablesql/TableSqlTest.java
@@ -1,8 +1,12 @@
 package io.github.streamingwithflink.test.tablesql;
 
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class TableSqlTest {
 
     public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
         
     }
 
